63454bce4b5403868dca7c96ccad666f0c936d82
pzaydulin <pzaydulin@gmail.com>
2024-11-28 18:02:19 -0800

    Add initial Angular project setup with Primeng and TailwindCSS
    The project includes:
    - A main application component with theme toggle functionality
    - Basic routing setup with default and master layouts
    - Sample pages for login, messages, and register
    - TailwindCSS configuration for dark and light themes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,34 @@
+import { Routes } from '@angular/router';
+import { MasterComponent } from './shared/layout/master/master.component';
+import { DefaultComponent } from './shared/layout/default/default.component';
+
+export const routes: Routes = [
+  //   { path: '', redirectTo: 'messages', pathMatch: 'full' },
+  {
+    path: '',
+    component: DefaultComponent,
+    providers: [],
+    children: [
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./pages/login/login.component').then((m) => m.LoginComponent),
+      },
+    ],
+  },
+  {
+    path: '',
+    component: MasterComponent,
+    providers: [],
+    children: [
+      {
+        path: 'messages',
+        loadComponent: () =>
+          import('./pages/messages/messages.component').then(
+            (m) => m.MessagesComponent
+          ),
+      },
+    ],
+  },
+  //   { path: '**', redirectTo: 'messages' },
+];
---
+++ src/app/shared/layout/master/master.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-master',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './master.component.html',
+  styleUrl: './master.component.css',
+})
+export class MasterComponent {}
